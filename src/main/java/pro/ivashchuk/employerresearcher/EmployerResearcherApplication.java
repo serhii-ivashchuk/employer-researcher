f66843c5c61ccfaec7429f9b27a5aaba1edaa7a6
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-06-07 18:06:00 +0200

    Add @SpringBootApplication
    This annotation combines another 3 annotations: @EnableAutoConfiguration - SB's auto-configuration machanism, @ConponentScan - scan on the package where the application is located, @Configuration -  allow to register extra beans in the context and/or import additional configuration classes.
    Body of the main method calls SpringApplicaton.run with two parameters - EmployerResearcherApplication.class and args array.

--- src/main/java/pro/ivashchuk/employerresearcher/EmployerResearcherApplication.java
+++ src/main/java/pro/ivashchuk/employerresearcher/EmployerResearcherApplication.java
@@ -1,7 +1,12 @@
 package pro.ivashchuk.employerresearcher;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class EmployerResearcherApplication {
 
     public static void main(String[] args) {
+        SpringApplication.run(EmployerResearcherApplication.class, args);
     }
 }
