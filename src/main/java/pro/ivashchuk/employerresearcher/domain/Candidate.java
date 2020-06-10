7183f3f106059992663f6e11ad595b681d80757d
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-06-10 18:04:42 +0200

    Add constructor
    Candidate entity needs constructor to instantiate instance of this class.

--- src/main/java/pro/ivashchuk/employerresearcher/domain/Candidate.java
+++ src/main/java/pro/ivashchuk/employerresearcher/domain/Candidate.java
@@ -11,4 +11,23 @@ public class Candidate {
     private String github;
     private String email;
     private String phone;
+
+    public Candidate(Long id, String name, String surname, String address,  String github, String email, String phone) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.address = address;
+        this.github = github;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public Candidate(String name, String surname, String address,  String github, String email, String phone) {
+        this.name = name;
+        this.surname = surname;
+        this.address = address;
+        this.github = github;
+        this.email = email;
+        this.phone = phone;
+    }
 }
