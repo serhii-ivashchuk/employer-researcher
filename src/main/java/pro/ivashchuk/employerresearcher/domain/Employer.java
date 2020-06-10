02264b2ae83b5cca7930d33ca645c729618b3c88
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-06-10 19:22:44 +0200

    Add properties to Employer
    Employer entity has name, full name, address, email, web site.

--- src/main/java/pro/ivashchuk/employerresearcher/domain/Employer.java
+++ src/main/java/pro/ivashchuk/employerresearcher/domain/Employer.java
@@ -8,4 +8,9 @@ public class Employer {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    private String name;
+    private String fullName;
+    private String address;
+    private String email;
+    private String webSite;
 }
