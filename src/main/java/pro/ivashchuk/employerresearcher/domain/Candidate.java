14c54c07e4173e9bd8ed7745ea31d319ab743b0f
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-06-10 18:02:37 +0200

    Add properties to Candidate
    Candidate entity has name, surname, address, email, phone, github, etc.

--- src/main/java/pro/ivashchuk/employerresearcher/domain/Candidate.java
+++ src/main/java/pro/ivashchuk/employerresearcher/domain/Candidate.java
@@ -5,4 +5,10 @@ import javax.persistence.Entity;
 @Entity
 public class Candidate {
 
+    private String name;
+    private String surname;
+    private String address;
+    private String github;
+    private String email;
+    private String phone;
 }
