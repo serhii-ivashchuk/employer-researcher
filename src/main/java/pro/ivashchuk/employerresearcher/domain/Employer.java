783ae96171d16e2e299e04031bbfaaa3036b555e
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-06-12 19:12:54 +0200

    Add Lombok annotations
    Lombok helps to reduce boiler plate code in Employer entity.

--- src/main/java/pro/ivashchuk/employerresearcher/domain/Employer.java
+++ src/main/java/pro/ivashchuk/employerresearcher/domain/Employer.java
@@ -1,12 +1,19 @@
 package pro.ivashchuk.employerresearcher.domain;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
+@Getter @Setter @NoArgsConstructor
 public class Employer {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Setter(AccessLevel.PROTECTED)
     private Long id;
     private String name;
     private String fullName;
