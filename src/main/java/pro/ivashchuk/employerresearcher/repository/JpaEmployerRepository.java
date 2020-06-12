6462abf745bdbce8df7e5ff76bb0f1612cd4eabf
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-06-12 19:08:10 +0200

    Add findAll(), findById(), save(), delete()
    These methods used to persist Employer entity.

--- src/main/java/pro/ivashchuk/employerresearcher/repository/JpaEmployerRepository.java
+++ src/main/java/pro/ivashchuk/employerresearcher/repository/JpaEmployerRepository.java
@@ -4,6 +4,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pro.ivashchuk.employerresearcher.domain.Employer;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface JpaEmployerRepository extends JpaRepository<Employer, Long> {
+
+    List<Employer> findAll();
+
+    Optional<Employer> findById(Long id);
+
+    Employer save(Employer Employer);
+
+    void delete(Employer Employer);
 }
