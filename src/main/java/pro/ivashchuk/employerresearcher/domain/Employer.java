8d97542344c9aeca0ad0a9229332991b2b25e17e
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-06-12 18:02:34 +0200

    Delete @Setter
    Deleted this annotation as unnecessary.

--- src/main/java/pro/ivashchuk/employerresearcher/domain/Employer.java
+++ src/main/java/pro/ivashchuk/employerresearcher/domain/Employer.java
@@ -1,7 +1,6 @@
 package pro.ivashchuk.employerresearcher.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -18,7 +17,6 @@ public class Employer implements Comparable<Employer> {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Setter(AccessLevel.PROTECTED)
     private Long id;
     private String name;
     private String fullName;
