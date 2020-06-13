1f768ae37b2c1de02684e0b34d5d4b8024db6685
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-06-13 18:01:56 +0200

    Add @JsonIgnore
    This annotation breaks recursion on the child side in one to many relation.

--- src/main/java/pro/ivashchuk/employerresearcher/domain/Employer.java
+++ src/main/java/pro/ivashchuk/employerresearcher/domain/Employer.java
@@ -1,5 +1,6 @@
 package pro.ivashchuk.employerresearcher.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -24,6 +25,7 @@ public class Employer implements Comparable<Employer> {
     private String address;
     private String email;
     private String website;
+    @JsonIgnore
     @ManyToOne
     private Candidate candidate;
 
