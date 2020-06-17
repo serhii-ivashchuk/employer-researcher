cc402c88287ac3782f6a215934392847c892ebe9
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-06-17 18:02:48 +0200

    Add getAllCandidates()
    This method will return "all_candidates" view on request get "/candidates".

--- src/main/java/pro/ivashchuk/employerresearcher/controller/CandidateController.java
+++ src/main/java/pro/ivashchuk/employerresearcher/controller/CandidateController.java
@@ -2,7 +2,14 @@ package pro.ivashchuk.employerresearcher.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import pro.ivashchuk.employerresearcher.domain.Candidate;
+import pro.ivashchuk.employerresearcher.repository.JpaCandidateRepository;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Controller
 @RequestMapping("/candidates")
@@ -10,4 +17,12 @@ public class CandidateController {
 
     @Autowired
     private JpaCandidateRepository jpaCandidateRepository;
+
+    @GetMapping
+    public String getAllCandidates(Model model) {
+        List<Candidate> candidates = new ArrayList<Candidate>(jpaCandidateRepository.findAll());
+        Collections.sort(candidates);
+        model.addAttribute("candidates", candidates);
+        return "all_candidates";
+    }
 }
