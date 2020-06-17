ffee9c3fd17ef38bf94802457622058b7d891c2c
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-06-17 18:01:18 +0200

    Add @Controller, @RequestMapping
    CandidateController is regular Spring controller annotated with @Controller.

--- src/main/java/pro/ivashchuk/employerresearcher/controller/CandidateController.java
+++ src/main/java/pro/ivashchuk/employerresearcher/controller/CandidateController.java
@@ -1,5 +1,10 @@
 package pro.ivashchuk.employerresearcher.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/candidates")
 public class CandidateController {
 
 }
