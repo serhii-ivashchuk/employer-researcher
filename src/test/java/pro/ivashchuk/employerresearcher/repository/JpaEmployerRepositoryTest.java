253ad523e89477695b058fb78a39a4912f7a4af7
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-06-18 19:00:18 +0200

    Add shouldFindEmployerById()
    This method tests repository's findById() method.

--- src/test/java/pro/ivashchuk/employerresearcher/repository/JpaEmployerRepositoryTest.java
+++ src/test/java/pro/ivashchuk/employerresearcher/repository/JpaEmployerRepositoryTest.java
@@ -3,14 +3,38 @@ package pro.ivashchuk.employerresearcher.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import pro.ivashchuk.employerresearcher.domain.Employer;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DataJpaTest
 class JpaEmployerRepositoryTest {
 
     @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
     JpaEmployerRepository jpaEmployerRepository;
 
     @Test
-    public void shouldFindEmployerById() {}
-
+    public void shouldFindEmployerById() {
+        // given
+        Employer testEmployer = new Employer(
+                "TheBestEmployer AG",
+                "TheBestEmployer Aktiengesellschaft",
+                "Marienplatz, 1, Munich, Germany",
+                "email@theBestEmployer.com",
+                "theBestEmployer.com");
+        entityManager.persist(testEmployer);
+        Long testEmployerId = (Long) entityManager.getId(testEmployer);
+        entityManager.flush();
+        // when
+        Employer foundEmployer = jpaEmployerRepository.findById(testEmployer.getId()).get();
+        // then
+        assertEquals(foundEmployer.getName(), testEmployer.getName());
+    }
 }
