7d07a0d8be1de4c849e40a80630f5d332147ed3c
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-06-18 19:13:17 +0200

    Add @Test shouldFindEmployerById()
    In this case, it is checked whether the object can be saved and whether it can be returned by the repository or not.

--- src/test/java/pro/ivashchuk/employerresearcher/repository/JpaEmployerRepositoryTest.java
+++ src/test/java/pro/ivashchuk/employerresearcher/repository/JpaEmployerRepositoryTest.java
@@ -1,14 +1,16 @@
 package pro.ivashchuk.employerresearcher.repository;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 class JpaEmployerRepositoryTest {
 
     @Autowired
     JpaEmployerRepository jpaEmployerRepository;
+
+    @Test
+    public void shouldFindEmployerById() {}
 
 }
