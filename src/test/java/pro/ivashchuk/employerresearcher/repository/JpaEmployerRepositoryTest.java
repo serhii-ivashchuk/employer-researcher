b6ed55b2202d1f138b59c4d1e3c6645b444b91f4
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-06-18 19:04:50 +0200

    Add @Autowired
    The Spring framework enables automatic dependency injection. Spring container can autowire relationships between collaborating beans. JpaEmployerRepository property is annotated with @Autowired. It will provide for test class a possibility to make assertions with the Employer entity.

--- src/test/java/pro/ivashchuk/employerresearcher/repository/JpaEmployerRepositoryTest.java
+++ src/test/java/pro/ivashchuk/employerresearcher/repository/JpaEmployerRepositoryTest.java
@@ -1,10 +1,14 @@
 package pro.ivashchuk.employerresearcher.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 class JpaEmployerRepositoryTest {
+
+    @Autowired
+    JpaEmployerRepository jpaEmployerRepository;
 
 }
