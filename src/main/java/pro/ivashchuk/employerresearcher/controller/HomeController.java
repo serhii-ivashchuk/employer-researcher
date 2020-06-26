e04c4c30abacb47eca9cf5927f46eef8d5ca8c64
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-06-26 19:10:46 +0200

    Add @RequestMapping, @GetMapping
    Annotated with @GetMapping method on request "/" will return view with name index.

--- src/main/java/pro/ivashchuk/employerresearcher/controller/HomeController.java
+++ src/main/java/pro/ivashchuk/employerresearcher/controller/HomeController.java
@@ -1,7 +1,15 @@
 package pro.ivashchuk.employerresearcher.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class HomeController {
+
+    @GetMapping("/")
+    public String returnHomePage() {
+        return "index";
+    }
 }
