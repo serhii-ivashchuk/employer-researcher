e3d68a8098ad285178e6122f5ed35111fe724b70
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-06-30 19:03:15 +0200

    Add @WebMvcTest
    With @WebMvcTest annotation the full Spring application context is started without the server. This annotation helps testing web layer.

--- src/test/java/pro/ivashchuk/employerresearcher/controller/HomeControllerTest.java
+++ src/test/java/pro/ivashchuk/employerresearcher/controller/HomeControllerTest.java
@@ -1,7 +1,10 @@
 package pro.ivashchuk.employerresearcher.controller;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@WebMvcTest(HomeController.class)
 class HomeControllerTest {
 
 }
