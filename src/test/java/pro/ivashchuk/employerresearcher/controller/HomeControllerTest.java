286a5954bffb433f031685df98438b2079173d9f
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-07-01 19:13:20 +0200

    Add testHomeControllerReturnsPageView()
    This method uses MockMvc collaborator. MockMvc starts servlet container and performs get request on "/" and check status, view and content of requested page.

--- src/test/java/pro/ivashchuk/employerresearcher/controller/HomeControllerTest.java
+++ src/test/java/pro/ivashchuk/employerresearcher/controller/HomeControllerTest.java
@@ -1,11 +1,27 @@
 package pro.ivashchuk.employerresearcher.controller;
 
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class HomeControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHomeControllerReturnsHomePageView() throws Exception{
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"))
+                .andExpect(content().string(CoreMatchers.containsString("Welcome to Employer Searcher App!")));
+    }
 }
