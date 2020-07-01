d575501fff0a83917e374c01ae0eefd09c08ec7b
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-07-01 19:09:40 +0200

    Add @SpringBootTest, @AutoConfigureMockMvc
    @SpringBootTest configures Spring application context. @AutoConfigureMockMvc enables and configure auto-configuration of MockMvc. MockMvc is a part of Spring MVC Test framework which helps in testing the controller explicitly starting a Servlet container.

--- src/test/java/pro/ivashchuk/employerresearcher/controller/HomeControllerTest.java
+++ src/test/java/pro/ivashchuk/employerresearcher/controller/HomeControllerTest.java
@@ -4,7 +4,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@WebMvcTest(HomeController.class)
-class HomeControllerTest {
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HomeControllerTest {
 
 }
