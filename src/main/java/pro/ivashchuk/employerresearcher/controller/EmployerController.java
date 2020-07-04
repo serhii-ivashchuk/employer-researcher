17aa98661dffdeada854f92ee00e18a11dd1a5ff
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-07-04 19:08:22 +0200

    Add @Autowired JpaEmployerRepository
    This controller uses JpaEmployerRepository to serve requests.

--- src/main/java/pro/ivashchuk/employerresearcher/controller/EmployerController.java
+++ src/main/java/pro/ivashchuk/employerresearcher/controller/EmployerController.java
@@ -1,9 +1,14 @@
 package pro.ivashchuk.employerresearcher.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pro.ivashchuk.employerresearcher.repository.JpaEmployerRepository;
 
 @Controller
 @RequestMapping("/employers")
 public class EmployerController {
+
+    @Autowired
+    JpaEmployerRepository jpaEmployerRepository;
 }
