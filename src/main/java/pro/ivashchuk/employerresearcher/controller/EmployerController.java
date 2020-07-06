4a141ef55d08ee022fd77f11a99bea0a8ba35ff3
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-07-06 19:07:52 +0200

    Add implementation to getAllEmployers()

--- src/main/java/pro/ivashchuk/employerresearcher/controller/EmployerController.java
+++ src/main/java/pro/ivashchuk/employerresearcher/controller/EmployerController.java
@@ -2,9 +2,15 @@ package pro.ivashchuk.employerresearcher.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pro.ivashchuk.employerresearcher.domain.Employer;
 import pro.ivashchuk.employerresearcher.repository.JpaEmployerRepository;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Controller
 @RequestMapping("/employers")
@@ -14,7 +20,10 @@ public class EmployerController {
     JpaEmployerRepository jpaEmployerRepository;
 
     @GetMapping
-    public String getAllEmployers() {
+    public String getAllEmployers(Model model) {
+        List<Employer> employers = new ArrayList<Employer>(jpaEmployerRepository.findAll());
+        Collections.sort(employers);
+        model.addAttribute("employers", employers);
         return "all_employers";
     }
 }
