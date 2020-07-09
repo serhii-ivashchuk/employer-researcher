1be6ee499cbecd50b11dca3a6b8373dcf407ca90
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-07-09 18:06:20 +0200

    Add jpaEmployerRepository, employer
    Test uses Employer entity and it's repository as collaborators to check that controller performs all request properly.

--- src/test/java/pro/ivashchuk/employerresearcher/controller/EmployerControllerTest.java
+++ src/test/java/pro/ivashchuk/employerresearcher/controller/EmployerControllerTest.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import pro.ivashchuk.employerresearcher.domain.Employer;
+import pro.ivashchuk.employerresearcher.repository.JpaEmployerRepository;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -11,4 +13,9 @@ class EmployerControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private JpaEmployerRepository jpaEmployerRepository;
+
+    private Employer employer;
 }
