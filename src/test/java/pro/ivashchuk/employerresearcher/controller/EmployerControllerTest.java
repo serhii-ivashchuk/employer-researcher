9270af00dd2a38da0a7802f14815b2b30af697a5
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-07-11 18:11:55 +0200

    Add testEmployerControllerReturnsAllEmployersPageView()
    MockMvc performs get “/employers”, expects status 200 is ok, expects view “all_employers” with content “All Employers”.

--- src/test/java/pro/ivashchuk/employerresearcher/controller/EmployerControllerTest.java
+++ src/test/java/pro/ivashchuk/employerresearcher/controller/EmployerControllerTest.java
@@ -1,6 +1,7 @@
 package pro.ivashchuk.employerresearcher.controller;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -8,6 +9,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import pro.ivashchuk.employerresearcher.domain.Employer;
 import pro.ivashchuk.employerresearcher.repository.JpaEmployerRepository;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -30,5 +35,13 @@ public class EmployerControllerTest {
                 "Marienplatz, 1, Munich, Germany",
                 "email@theBestEmployer.com",
                 "theBestEmployer.com");
+    }
+
+    @Test
+    public void testEmployerControllerReturnsAllEmployersPageView() throws Exception {
+        mockMvc.perform(get("/employers"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("all_employers"))
+                .andExpect(content().string(containsString("All Employers")));
     }
 }
