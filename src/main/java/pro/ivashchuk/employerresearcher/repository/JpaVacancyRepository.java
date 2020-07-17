218d0ca67d7b5af71657a75fe88c5ae557c725f1
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-07-17 18:06:38 +0200

    Add findAll(), findById(), save(), delete(), deleteById().
    These methods are been used to persist Vacancy entity.

--- src/main/java/pro/ivashchuk/employerresearcher/repository/JpaVacancyRepository.java
+++ src/main/java/pro/ivashchuk/employerresearcher/repository/JpaVacancyRepository.java
@@ -2,8 +2,22 @@ package pro.ivashchuk.employerresearcher.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import pro.ivashchuk.employerresearcher.domain.Employer;
 import pro.ivashchuk.employerresearcher.domain.Vacancy;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface JpaVacancyRepository extends JpaRepository<Vacancy, Long> {
+
+    List<Vacancy> findAll();
+
+    Optional<Vacancy> findById(Long id);
+
+    Vacancy save(Vacancy vacancy);
+
+    void delete(Vacancy vacancy);
+
+    void deleteById(Long id);
 }
