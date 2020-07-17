58cd4c8cfd461c7f961cc1744dbf20483a396f65
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-07-17 18:01:59 +0200

    Fix typo
    This repository operates with Vacancy domain not Employer domain.

--- src/main/java/pro/ivashchuk/employerresearcher/repository/JpaVacancyRepository.java
+++ src/main/java/pro/ivashchuk/employerresearcher/repository/JpaVacancyRepository.java
@@ -2,7 +2,6 @@ package pro.ivashchuk.employerresearcher.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import pro.ivashchuk.employerresearcher.domain.Employer;
 import pro.ivashchuk.employerresearcher.domain.Vacancy;
 
 import java.util.List;
