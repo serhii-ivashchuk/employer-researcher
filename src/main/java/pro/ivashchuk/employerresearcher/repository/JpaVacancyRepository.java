932b2ad244e1a88a5df81f1fc892f4afb32230a7
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-07-17 18:04:53 +0200

    Add @Repository
    @Repository annotation indicates that class provides CRUD operations.

--- src/main/java/pro/ivashchuk/employerresearcher/repository/JpaVacancyRepository.java
+++ src/main/java/pro/ivashchuk/employerresearcher/repository/JpaVacancyRepository.java
@@ -1,4 +1,9 @@
 package pro.ivashchuk.employerresearcher.repository;
 
-public interface JpaVacancyRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pro.ivashchuk.employerresearcher.domain.Vacancy;
+
+@Repository
+public interface JpaVacancyRepository extends JpaRepository<Vacancy, Long> {
 }
