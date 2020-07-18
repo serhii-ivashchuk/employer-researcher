84f54dad5fe93bef5a37343817b3f1225e3a136b
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-07-18 18:03:43 +0200

    Add @DataJpaTest
    @DataJpaTest annotation focuses only on JPA components. This annotation will disable full auto-configuration and instead apply only configuration relevant to JPA tests. When using JUnit 5, this annotation contains the statement @ExtendWith(SpringExtension.class), so no additional runner definition is required.

--- src/test/java/pro/ivashchuk/employerresearcher/repository/JpaVacancyRepositoryTest.java
+++ src/test/java/pro/ivashchuk/employerresearcher/repository/JpaVacancyRepositoryTest.java
@@ -1,5 +1,8 @@
 package pro.ivashchuk.employerresearcher.repository;
 
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
 class JpaVacancyRepositoryTest {
 
 }
