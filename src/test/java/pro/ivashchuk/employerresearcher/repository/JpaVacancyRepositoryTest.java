9cea72b0a5031ecbaec2295f6e3bb99a15f2cc6b
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-07-18 18:32:52 +0200

    Add shouldFindAllVacancies()
    This method tests repository's findAllVacancies() method.

--- src/test/java/pro/ivashchuk/employerresearcher/repository/JpaVacancyRepositoryTest.java
+++ src/test/java/pro/ivashchuk/employerresearcher/repository/JpaVacancyRepositoryTest.java
@@ -1,8 +1,15 @@
 package pro.ivashchuk.employerresearcher.repository;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import pro.ivashchuk.employerresearcher.domain.Vacancy;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DataJpaTest
 class JpaVacancyRepositoryTest {
@@ -13,4 +20,29 @@ class JpaVacancyRepositoryTest {
     @Autowired
     JpaVacancyRepository jpaVacancyRepository;
 
+    @Test
+    public void shouldFindAllVacancies() {
+        Vacancy testVacancy1 = getVacancy("Junior Java Engineer");
+        Vacancy testVacancy2 = getVacancy("Java Engineer");
+        Vacancy testVacancy3 = getVacancy("Senior Java Engineer");
+        List<Vacancy> initialEmployers = Arrays.asList(testVacancy1, testVacancy2, testVacancy3);
+        entityManager.persist(testVacancy1);
+        entityManager.persist(testVacancy2);
+        entityManager.persist(testVacancy3);
+        entityManager.flush();
+        int vacanciesQuantity = jpaVacancyRepository.findAll().size();
+        List<Vacancy> foundVacancies = jpaVacancyRepository.findAll();
+
+        assertEquals(foundVacancies.size(), vacanciesQuantity, "When repository stores 3 vacancies, then it " +
+                "should return list of 3 Vacancies");
+    }
+
+    private Vacancy getVacancy(String position) {
+        return new Vacancy(
+                position,
+                "theBestEmployer.com/vacancies/1",
+                "Notes: this position is relevant to my search",
+                "email@theBestEmployer.com",
+                "Comment: it's very suitable");
+    }
 }
