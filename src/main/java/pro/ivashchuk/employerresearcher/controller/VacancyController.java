47274eb9ebd098a368d248a108cc584d63e3466a
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-07-19 18:04:21 +0200

    Add postNewVacancy()
    This method will return "redirect:/vacancies" view on request post "/vacancies/vacancy/{id}/change". The Vacancy is
    processed by this method from previously requested form and posted on before mentioned post request address.

--- src/main/java/pro/ivashchuk/employerresearcher/controller/VacancyController.java
+++ src/main/java/pro/ivashchuk/employerresearcher/controller/VacancyController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pro.ivashchuk.employerresearcher.domain.Vacancy;
 import pro.ivashchuk.employerresearcher.repository.JpaVacancyRepository;
@@ -38,5 +39,11 @@ public class VacancyController {
     public String getAddNewVacancy(Model model) {
         model.addAttribute("vacancy", new Vacancy());
         return "add_vacancy";
+    }
+
+    @PostMapping("/addNewVacancy")
+    public String postNewVacancy(Vacancy vacancy) {
+        jpaVacancyRepository.save(vacancy);
+        return "redirect:/vacancies";
     }
 }
