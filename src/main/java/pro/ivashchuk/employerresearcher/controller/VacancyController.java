62879f6cf5646ce44ecb571588d05207b39e2b68
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-07-19 18:02:47 +0200

    Add @Autowired JpaVacancyRepository
    This controller uses JpaVacancyRepository to serve requests.

--- src/main/java/pro/ivashchuk/employerresearcher/controller/VacancyController.java
+++ src/main/java/pro/ivashchuk/employerresearcher/controller/VacancyController.java
@@ -1,9 +1,14 @@
 package pro.ivashchuk.employerresearcher.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pro.ivashchuk.employerresearcher.repository.JpaVacancyRepository;
 
 @Controller
 @RequestMapping("/vacancies")
 public class VacancyController {
+
+    @Autowired
+    private JpaVacancyRepository jpaVacancyRepository;
 }
