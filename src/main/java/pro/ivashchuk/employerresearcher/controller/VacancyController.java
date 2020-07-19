2428f5974953b3c81637f4f278a1011ad358b792
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-07-19 18:00:41 +0200

    Add getAllVacancies()
    This method will return "all_vacancies" view on request get "/vacancies".

--- src/main/java/pro/ivashchuk/employerresearcher/controller/VacancyController.java
+++ src/main/java/pro/ivashchuk/employerresearcher/controller/VacancyController.java
@@ -2,8 +2,15 @@ package pro.ivashchuk.employerresearcher.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pro.ivashchuk.employerresearcher.domain.Vacancy;
 import pro.ivashchuk.employerresearcher.repository.JpaVacancyRepository;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Controller
 @RequestMapping("/vacancies")
@@ -11,4 +18,12 @@ public class VacancyController {
 
     @Autowired
     private JpaVacancyRepository jpaVacancyRepository;
+
+    @GetMapping
+    public String getAllVacancies(Model model) {
+        List<Vacancy> vacancies = new ArrayList<Vacancy>(jpaVacancyRepository.findAll());
+        Collections.sort(vacancies);
+        model.addAttribute("vacancies", vacancies);
+        return "all_vacancies";
+    }
 }
