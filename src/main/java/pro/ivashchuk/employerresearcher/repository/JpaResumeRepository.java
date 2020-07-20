aecb81ae3f86df13391f3e10105f7d27150f880b
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-07-20 18:10:28 +0200

    Add extends JpaRepository
    This interface extends Spring JpaRepository.

--- src/main/java/pro/ivashchuk/employerresearcher/repository/JpaResumeRepository.java
+++ src/main/java/pro/ivashchuk/employerresearcher/repository/JpaResumeRepository.java
@@ -1,5 +1,6 @@
 package pro.ivashchuk.employerresearcher.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pro.ivashchuk.employerresearcher.domain.Resume;
 
@@ -7,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface JpaResumeRepository {
+public interface JpaResumeRepository extends JpaRepository<Resume, Long> {
 
         List<Resume> findAll();
 
