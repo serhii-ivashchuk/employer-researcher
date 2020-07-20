3dd3ed01d2704f64e4e676df1f467cd1b248d0c4
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-07-20 18:08:29 +0200

    Add findAll(), findById(), save(), delete(), deleteById().
    These methods are been used to persist Resume entity.

--- src/main/java/pro/ivashchuk/employerresearcher/repository/JpaResumeRepository.java
+++ src/main/java/pro/ivashchuk/employerresearcher/repository/JpaResumeRepository.java
@@ -1,7 +1,21 @@
 package pro.ivashchuk.employerresearcher.repository;
 
 import org.springframework.stereotype.Repository;
+import pro.ivashchuk.employerresearcher.domain.Resume;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface JpaResumeRepository {
+
+        List<Resume> findAll();
+
+        Optional<Resume> findById(Long id);
+
+        Resume save(Resume resume);
+
+        void delete(Resume resume);
+
+        void deleteById(Long id);
 }
