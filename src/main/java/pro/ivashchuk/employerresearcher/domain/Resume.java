c0b82e743dd39d99b9644e01ae586d162728cc1a
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-07-20 18:02:23 +0200

    Add id attribute
    Attribute id is annotated with @id and JPA recognizes it as the object’s ID. @GeneratedValue(strategy = GenerationType.AUTO) used to indicate that the ID should be generated automatically.

--- src/main/java/pro/ivashchuk/employerresearcher/domain/Resume.java
+++ src/main/java/pro/ivashchuk/employerresearcher/domain/Resume.java
@@ -1,8 +1,15 @@
 package pro.ivashchuk.employerresearcher.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Resume {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
 }
