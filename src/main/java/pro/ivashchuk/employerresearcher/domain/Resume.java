4797448bbfb86105242f6f49f99f62439cddad8b
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-07-20 18:11:59 +0200

    Add Lombok annotations
    Lombok helps to reduce boiler plate code in Resume entity.

--- src/main/java/pro/ivashchuk/employerresearcher/domain/Resume.java
+++ src/main/java/pro/ivashchuk/employerresearcher/domain/Resume.java
@@ -1,15 +1,24 @@
 package pro.ivashchuk.employerresearcher.domain;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Resume {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Setter(AccessLevel.PROTECTED)
     private Long id;
     private String name;
     private String date;
