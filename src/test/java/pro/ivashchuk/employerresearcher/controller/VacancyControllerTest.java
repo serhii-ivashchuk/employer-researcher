37052d1edd9c2572d71aad2394f2180a6af47811
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-07-23 18:24:13 +0200

    Add @BeforeEach setup()
    Before each test runs the vacancy is being set up.

--- src/test/java/pro/ivashchuk/employerresearcher/controller/VacancyControllerTest.java
+++ src/test/java/pro/ivashchuk/employerresearcher/controller/VacancyControllerTest.java
@@ -1,5 +1,6 @@
 package pro.ivashchuk.employerresearcher.controller;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -20,5 +21,17 @@ class VacancyControllerTest {
     private JpaVacancyRepository jpaVacancyRepository;
 
     private Vacancy vacancy;
+
+    @BeforeEach
+    public  void setUp() {
+        vacancy = new Vacancy(
+                "Junior Java Engineer",
+                "theBestEmployer.com/vacancies/1",
+                "Notes: this position is relevant to my search",
+                "theBestEmployer is hiring now! For description please visit our website theBestEmployer" +
+                        ".com/vacancies.",
+                "Comment: it's very suitable"
+        );
+    }
 
 }
