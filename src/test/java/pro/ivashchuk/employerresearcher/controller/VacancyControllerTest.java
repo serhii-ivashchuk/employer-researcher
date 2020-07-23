709040c15fe1502a1b80b4ea49aad31203ef9bfe
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-07-23 18:19:36 +0200

    Add jpaVacancyRepository, vacancy
    Test uses Vacancy entity and it's repository to check that controller performs request properly.

--- src/test/java/pro/ivashchuk/employerresearcher/controller/VacancyControllerTest.java
+++ src/test/java/pro/ivashchuk/employerresearcher/controller/VacancyControllerTest.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import pro.ivashchuk.employerresearcher.domain.Vacancy;
+import pro.ivashchuk.employerresearcher.repository.JpaVacancyRepository;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -11,5 +13,10 @@ class VacancyControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private JpaVacancyRepository jpaVacancyRepository;
+
+    private Vacancy vacancy;
 
 }
