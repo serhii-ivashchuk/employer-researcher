f03c3b531de2149fe5f257cec78c05bc6627935a
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-07-26 18:07:39 +0200

    Add postNewResume()
    This method will return "redirect:/resumes" view on request post "/resumes/resume/{id}/change". The Resume is processed by this method from  previously requested form and posted on before mentioned post address.

--- src/main/java/pro/ivashchuk/employerresearcher/controller/ResumeController.java
+++ src/main/java/pro/ivashchuk/employerresearcher/controller/ResumeController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pro.ivashchuk.employerresearcher.domain.Resume;
 import pro.ivashchuk.employerresearcher.repository.JpaResumeRepository;
@@ -38,5 +39,11 @@ public class ResumeController {
     public String getAddNewResume(Model model) {
         model.addAttribute("resume", new Resume());
         return "add_resume";
+    }
+
+    @PostMapping("/addNewResume")
+    public String postNewResume(Resume resume) {
+        jpaResumeRepository.save(resume);
+        return "redirect:/resumes";
     }
 }
