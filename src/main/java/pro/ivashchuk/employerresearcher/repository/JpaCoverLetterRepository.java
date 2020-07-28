17ba4d5f88529bee1ca636291caf2bf9c727d57c
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-07-28 18:03:07 +0200

    Add findAll(), findById(), save(), delete(), deleteById().
    These methods are been used to persist CoverLetter entity.

--- src/main/java/pro/ivashchuk/employerresearcher/repository/JpaCoverLetterRepository.java
+++ src/main/java/pro/ivashchuk/employerresearcher/repository/JpaCoverLetterRepository.java
@@ -1,7 +1,22 @@
 package pro.ivashchuk.employerresearcher.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import pro.ivashchuk.employerresearcher.domain.CoverLetter;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
-public interface JpaCoverLetterRepository {
+public interface JpaCoverLetterRepository extends JpaRepository<CoverLetter, Long> {
+
+    List<CoverLetter> findAll();
+
+    Optional<CoverLetter> findById(Long id);
+
+    CoverLetter save(CoverLetter coverLetter);
+
+    void delete(CoverLetter coverLetter);
+
+    void deleteById(Long id);
 }
