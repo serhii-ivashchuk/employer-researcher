5b243bce9f8f968542a13461a959a63e7975d7e1
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-07-28 18:01:46 +0200

    Add @Autowired private MockMvc
    Spring can handle the incoming HTTP request without starting a server at all, to test only the layer below that. That way, almost of the full stack is used, and code will be called in exactly the same way as if it were processing a real HTTP request but without the cost of starting the server. MockMvc is used to do that.

--- src/test/java/pro/ivashchuk/employerresearcher/controller/ResumeControllerTest.java
+++ src/test/java/pro/ivashchuk/employerresearcher/controller/ResumeControllerTest.java
@@ -1,12 +1,17 @@
 package pro.ivashchuk.employerresearcher.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class ResumeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
 
 }
