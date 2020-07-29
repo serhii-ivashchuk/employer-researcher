a0fd22c28c5104afddd1a9cb7772ea522e7d7754
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-07-29 18:00:15 +0200

    Delete @Setter
    Deleted this annotation as unnecessary.

--- src/main/java/pro/ivashchuk/employerresearcher/domain/CoverLetter.java
+++ src/main/java/pro/ivashchuk/employerresearcher/domain/CoverLetter.java
@@ -1,7 +1,6 @@
 package pro.ivashchuk.employerresearcher.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,7 +15,6 @@ public class CoverLetter implements Comparable<CoverLetter> {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Setter(AccessLevel.PROTECTED)
     private Long id;
     private String name;
     private String date;
