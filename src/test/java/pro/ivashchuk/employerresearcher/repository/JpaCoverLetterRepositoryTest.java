ba789e9e55d22b975fa1fdc3c12f47bfdcb922f8
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-07-30 18:03:08 +0200

    Add @Autowired collaborators …
    The Spring framework enables automatic dependency injection. Spring container can autowire relationships between collaborating beans. TestEntityManager and JpaCoverLetterRepository properties are annotated with @Autowired. It will provide for test class a possibility to use EntityManager and to make assertions with the CoverLetter entity.

--- src/test/java/pro/ivashchuk/employerresearcher/repository/JpaCoverLetterRepositoryTest.java
+++ src/test/java/pro/ivashchuk/employerresearcher/repository/JpaCoverLetterRepositoryTest.java
@@ -1,8 +1,15 @@
 package pro.ivashchuk.employerresearcher.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
 @DataJpaTest
 class JpaCoverLetterRepositoryTest {
 
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    JpaCoverLetterRepository jpaCoverLetterRepository;
 }
