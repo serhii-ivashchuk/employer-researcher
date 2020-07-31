fec9cb39bfe4de6d63f26fc4f87280c66d2b4889
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-07-31 18:02:38 +0200

    Add getAllCoverLetters()
    This method will return "all_cover_letters" view on request get "/coverLetters".

--- src/main/java/pro/ivashchuk/employerresearcher/controller/CoverLetterController.java
+++ src/main/java/pro/ivashchuk/employerresearcher/controller/CoverLetterController.java
@@ -2,8 +2,15 @@ package pro.ivashchuk.employerresearcher.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pro.ivashchuk.employerresearcher.domain.CoverLetter;
 import pro.ivashchuk.employerresearcher.repository.JpaCoverLetterRepository;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Controller
 @RequestMapping("/coverLetters")
@@ -11,4 +18,12 @@ public class CoverLetterController {
 
     @Autowired
     private JpaCoverLetterRepository jpaCoverLetterRepository;
+
+    @GetMapping
+    public String getAllCoverLetters(Model model) {
+        List<CoverLetter> coverLetters = new ArrayList<CoverLetter>(jpaCoverLetterRepository.findAll());
+        Collections.sort(coverLetters);
+        model.addAttribute("coverLetters", coverLetters);
+        return "all_cover_letters";
+    }
 }
