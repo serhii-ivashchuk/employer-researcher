8d263de98c41b645693e03edb5e32997c2eb2db4
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-07-31 18:03:27 +0200

    Add getCoverLetterById()
    This method will return "coverLetter" view on request get "/coverLetters/coverLetter/{id}".

--- src/main/java/pro/ivashchuk/employerresearcher/controller/CoverLetterController.java
+++ src/main/java/pro/ivashchuk/employerresearcher/controller/CoverLetterController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pro.ivashchuk.employerresearcher.domain.CoverLetter;
 import pro.ivashchuk.employerresearcher.repository.JpaCoverLetterRepository;
@@ -25,5 +26,11 @@ public class CoverLetterController {
         Collections.sort(coverLetters);
         model.addAttribute("coverLetters", coverLetters);
         return "all_cover_letters";
+    }
+
+    @GetMapping("/coverLetter/{id}")
+    public String getCoverLetterById(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("coverLetter", jpaCoverLetterRepository.findById(id).get());
+        return "coverLetter";
     }
 }
