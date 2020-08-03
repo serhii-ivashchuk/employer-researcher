2668fe6039bb3d81db024dbe6820f7143e490f66
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-08-03 18:13:17 +0200

    Add jpaCoverLetterRepository, coverLetter
    Test uses CoverLetter entity and it's repository to check that controller performs request properly.

--- src/test/java/pro/ivashchuk/employerresearcher/controller/CoverLetterControllerTest.java
+++ src/test/java/pro/ivashchuk/employerresearcher/controller/CoverLetterControllerTest.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import pro.ivashchuk.employerresearcher.domain.CoverLetter;
+import pro.ivashchuk.employerresearcher.repository.JpaCoverLetterRepository;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -11,4 +13,9 @@ class CoverLetterControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private JpaCoverLetterRepository jpaCoverLetterRepository;
+
+    private CoverLetter coverLetter;
 }
