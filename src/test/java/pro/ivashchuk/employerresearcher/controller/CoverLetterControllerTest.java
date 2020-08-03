809522fc3b8277a0be7ddbd4c106d6386bc7c56e
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-08-03 18:14:20 +0200

    Add @BeforeEach setup()
    Before each test runs the coverLetter is being set up.

--- src/test/java/pro/ivashchuk/employerresearcher/controller/CoverLetterControllerTest.java
+++ src/test/java/pro/ivashchuk/employerresearcher/controller/CoverLetterControllerTest.java
@@ -1,5 +1,6 @@
 package pro.ivashchuk.employerresearcher.controller;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -20,4 +21,10 @@ class CoverLetterControllerTest {
     private JpaCoverLetterRepository jpaCoverLetterRepository;
 
     private CoverLetter coverLetter;
+
+    @BeforeEach
+    public  void setUp() {
+        coverLetter = new CoverLetter(
+        );
+    }
 }
