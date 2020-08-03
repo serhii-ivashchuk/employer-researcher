84a4443f786026918344fa3fb199602bd07aa731
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-08-03 18:13:52 +0200

    Add @TestInstance
    @TestInstance annotation is used when test need some setup @Before and @After all tests run.

--- src/test/java/pro/ivashchuk/employerresearcher/controller/CoverLetterControllerTest.java
+++ src/test/java/pro/ivashchuk/employerresearcher/controller/CoverLetterControllerTest.java
@@ -1,5 +1,6 @@
 package pro.ivashchuk.employerresearcher.controller;
 
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,6 +10,7 @@ import pro.ivashchuk.employerresearcher.repository.JpaCoverLetterRepository;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class CoverLetterControllerTest {
 
     @Autowired
