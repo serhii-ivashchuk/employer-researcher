baaedd0d744f8e659ec50e93d53074d4e9d998b0
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-09-01 18:06:53 +0200

    Add JpaEmployerRepository
    EmployerRestController uses JpaEmployerRepository to perform CRUD operations with employer entity.

--- src/main/java/pro/ivashchuk/employerresearcher/rest/EmployerRestController.java
+++ src/main/java/pro/ivashchuk/employerresearcher/rest/EmployerRestController.java
@@ -1,11 +1,20 @@
 package pro.ivashchuk.employerresearcher.rest;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pro.ivashchuk.employerresearcher.domain.Employer;
+import pro.ivashchuk.employerresearcher.repository.JpaEmployerRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping(value = "/api/employers")
 public class EmployerRestController {
+
+    @Autowired
+    private JpaEmployerRepository jpaEmployerRepository;
 }
