d98fb7896062375b319a338bbe10bf49a75f8c2d
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-09-09 18:02:30 +0200

    Add @RestController, @RequestMapping and @CrossOrigin
    First annotation is similar to @Controller, second is the same as before, the last one allows to take requests from angular on port 4200.

--- src/main/java/pro/ivashchuk/employerresearcher/rest/VacancyRestController.java
+++ src/main/java/pro/ivashchuk/employerresearcher/rest/VacancyRestController.java
@@ -1,4 +1,11 @@
 package pro.ivashchuk.employerresearcher.rest;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping(value = "/api/vacancies")
 public class VacancyRestController {
 }
