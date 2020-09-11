9722afd3b2a4b09ebcfe53775baffa8402b82cc1
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-09-11 18:02:49 +0200

    Add getAllVacancies
    This method will serve all vacancies request and will return json with all vacancies.

--- src/main/java/pro/ivashchuk/employerresearcher/rest/VacancyRestController.java
+++ src/main/java/pro/ivashchuk/employerresearcher/rest/VacancyRestController.java
@@ -2,9 +2,13 @@ package pro.ivashchuk.employerresearcher.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pro.ivashchuk.employerresearcher.domain.Vacancy;
 import pro.ivashchuk.employerresearcher.repository.JpaVacancyRepository;
+
+import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -13,4 +17,9 @@ public class VacancyRestController {
 
     @Autowired
     private JpaVacancyRepository jpaVacancyRepository;
+
+    @GetMapping(produces = "application/json")
+    public List<Vacancy> getAllVacancies() {
+        return jpaVacancyRepository.findAll();
+    }
 }
