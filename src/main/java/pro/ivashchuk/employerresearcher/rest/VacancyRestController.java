7fd2ade58f6f0555d0e9bff18397d40b1c34da74
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-09-11 18:01:30 +0200

    Add JpaVacancyRepository
    VacancyRestController uses JpaVacancyRepository to perform CRUD operations with vacancy entity.

--- src/main/java/pro/ivashchuk/employerresearcher/rest/VacancyRestController.java
+++ src/main/java/pro/ivashchuk/employerresearcher/rest/VacancyRestController.java
@@ -1,11 +1,16 @@
 package pro.ivashchuk.employerresearcher.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pro.ivashchuk.employerresearcher.repository.JpaVacancyRepository;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping(value = "/api/vacancies")
 public class VacancyRestController {
+
+    @Autowired
+    private JpaVacancyRepository jpaVacancyRepository;
 }
