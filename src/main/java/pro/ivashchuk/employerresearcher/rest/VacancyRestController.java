7f663ad04f4c66a0aaf0b6d1aabca1004914e253
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-09-12 18:01:47 +0200

    Add getVacancyById
    This method will serve get vacancy by id request and will return json with vacancy by it's id.

--- src/main/java/pro/ivashchuk/employerresearcher/rest/VacancyRestController.java
+++ src/main/java/pro/ivashchuk/employerresearcher/rest/VacancyRestController.java
@@ -1,10 +1,7 @@
 package pro.ivashchuk.employerresearcher.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pro.ivashchuk.employerresearcher.domain.Vacancy;
 import pro.ivashchuk.employerresearcher.repository.JpaVacancyRepository;
 
@@ -21,5 +18,10 @@ public class VacancyRestController {
     @GetMapping(produces = "application/json")
     public List<Vacancy> getAllVacancies() {
         return jpaVacancyRepository.findAll();
+    }
+
+    @GetMapping(path = "/vacancy/{id}", produces = "application/json")
+    public Vacancy getVacancyById(@PathVariable("id") Long id) {
+        return jpaVacancyRepository.findById(id).get();
     }
 }
