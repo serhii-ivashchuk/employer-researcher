3547f052221f55cd0f61d273bf2ad726c59dd46b
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-09-15 18:01:12 +0200

    Add updateVacancy
    This method will serve update vacancy request and will return http status is ok or not found if vacancy will be not found.

--- src/main/java/pro/ivashchuk/employerresearcher/rest/VacancyRestController.java
+++ src/main/java/pro/ivashchuk/employerresearcher/rest/VacancyRestController.java
@@ -2,11 +2,13 @@ package pro.ivashchuk.employerresearcher.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pro.ivashchuk.employerresearcher.domain.Vacancy;
 import pro.ivashchuk.employerresearcher.repository.JpaVacancyRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -30,5 +32,18 @@ public class VacancyRestController {
     public HttpStatus addNewVacancy(@RequestBody Vacancy vacancy) {
         jpaVacancyRepository.save(vacancy);
         return HttpStatus.CREATED;
+    }
+
+    //patch or put mapping
+    @PatchMapping("/vacancy/{id}/update")
+    public ResponseEntity<Vacancy> updateVacancy(@PathVariable("id") Long id,
+                                                Vacancy updatedVacancy) {
+        Optional<Vacancy> vacancyData = jpaVacancyRepository.findById(id);
+        if (vacancyData.isPresent()) {
+            Vacancy vacancyToUpdate = vacancyData.get();
+            return new ResponseEntity<>(jpaVacancyRepository.save(vacancyToUpdate), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
