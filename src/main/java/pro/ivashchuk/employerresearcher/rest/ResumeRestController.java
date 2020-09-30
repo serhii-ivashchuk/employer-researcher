d925c425e9191871a360d8ffea3d6d8b591c8c83
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-09-30 18:08:06 +0200

    Add JpaResumeRepository
    ResumeRestController uses JpaResumeRepository to perform CRUD operations with resume entity.

--- src/main/java/pro/ivashchuk/employerresearcher/rest/ResumeRestController.java
+++ src/main/java/pro/ivashchuk/employerresearcher/rest/ResumeRestController.java
@@ -1,12 +1,16 @@
 package pro.ivashchuk.employerresearcher.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pro.ivashchuk.employerresearcher.repository.JpaResumeRepository;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping(value = "/api/resumes")
 public class ResumeRestController {
 
+    @Autowired
+    private JpaResumeRepository jpaResumeRepository;
 }
