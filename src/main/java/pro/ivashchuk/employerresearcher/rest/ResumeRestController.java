26f64d2434eea1d7912ce3b1027dafb70d9ae926
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-10-02 18:01:55 +0200

    Add getAllResumes
    This method will serve all resumes, request and will return json with all resumes.

--- src/main/java/pro/ivashchuk/employerresearcher/rest/ResumeRestController.java
+++ src/main/java/pro/ivashchuk/employerresearcher/rest/ResumeRestController.java
@@ -2,9 +2,13 @@ package pro.ivashchuk.employerresearcher.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pro.ivashchuk.employerresearcher.domain.Resume;
 import pro.ivashchuk.employerresearcher.repository.JpaResumeRepository;
+
+import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -13,4 +17,9 @@ public class ResumeRestController {
 
     @Autowired
     private JpaResumeRepository jpaResumeRepository;
+
+    @GetMapping(produces = "application/json")
+    public List<Resume> getAllResumes() {
+        return jpaResumeRepository.findAll();
+    }
 }
