003c0c339aaac3a14c42d28ce4b3d39f81c24f4e
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-10-05 18:00:45 +0200

    Add updateResume
    This method will serve update resume request and will return http status is ok or not found if resume will be not found.

--- src/main/java/pro/ivashchuk/employerresearcher/rest/ResumeRestController.java
+++ src/main/java/pro/ivashchuk/employerresearcher/rest/ResumeRestController.java
@@ -2,6 +2,7 @@ package pro.ivashchuk.employerresearcher.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pro.ivashchuk.employerresearcher.domain.Resume;
 import pro.ivashchuk.employerresearcher.domain.Vacancy;
@@ -9,6 +10,7 @@ import pro.ivashchuk.employerresearcher.repository.JpaResumeRepository;
 import pro.ivashchuk.employerresearcher.repository.JpaVacancyRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -40,5 +42,17 @@ public class ResumeRestController {
         resume.setVacancy(vacancy);
         jpaResumeRepository.save(resume);
         return HttpStatus.CREATED;
+    }
+
+    @PatchMapping("/resume/{id}/update")
+    public ResponseEntity<Resume> putResume(@PathVariable("id") Long id,
+                                               Resume updatedResume) {
+        Optional<Resume> resumeData = jpaResumeRepository.findById(id);
+        if (resumeData.isPresent()) {
+            Resume resumeToUpdate = resumeData.get();
+            return new ResponseEntity<>(jpaResumeRepository.save(resumeToUpdate), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
