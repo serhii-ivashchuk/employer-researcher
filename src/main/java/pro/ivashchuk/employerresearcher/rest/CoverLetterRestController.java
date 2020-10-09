5951f3ddfa122e7fcf83a720cd5585a6922d3128
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-10-09 18:01:40 +0200

    Add getAllCoverLetters
    This method will serve all cover letters, request and will return json with all cover letters.

--- src/main/java/pro/ivashchuk/employerresearcher/rest/CoverLetterRestController.java
+++ src/main/java/pro/ivashchuk/employerresearcher/rest/CoverLetterRestController.java
@@ -2,8 +2,10 @@ package pro.ivashchuk.employerresearcher.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pro.ivashchuk.employerresearcher.domain.CoverLetter;
 import pro.ivashchuk.employerresearcher.repository.JpaCoverLetterRepository;
 
 @CrossOrigin(origins = "http://localhost:4200")
@@ -13,4 +15,9 @@ public class CoverLetterRestController {
 
 	@Autowired
 	private JpaCoverLetterRepository jpaCoverLetterRepository;
+
+	@GetMapping(produces = "application/json")
+	public List<CoverLetter> getAllCoverLetters() {
+		return jpaCoverLetterRepository.findAll();
+	}
 }
