3f5f604929081ee272b202235333bcea403164eb
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-10-14 18:02:55 +0200

    Add necessary import
    Just added Optional.

--- src/main/java/pro/ivashchuk/employerresearcher/rest/CoverLetterRestController.java
+++ src/main/java/pro/ivashchuk/employerresearcher/rest/CoverLetterRestController.java
@@ -9,6 +9,8 @@ import pro.ivashchuk.employerresearcher.domain.Vacancy;
 import pro.ivashchuk.employerresearcher.repository.JpaCoverLetterRepository;
 import pro.ivashchuk.employerresearcher.repository.JpaVacancyRepository;
 
+import java.util.Optional;
+
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping(value = "/api/coverLetters")
