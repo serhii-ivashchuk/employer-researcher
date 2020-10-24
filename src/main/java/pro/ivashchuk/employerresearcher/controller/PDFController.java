28c4eec17e9c5585c2749561cee4cc24333dcc16
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-10-24 18:03:41 +0200

    Add PDFService pdfService
    PDFService is responsible for pdf creation. PDFController receives PDFrequests, than calls PDFService which returns prepared PDF document.

--- src/main/java/pro/ivashchuk/employerresearcher/controller/PDFController.java
+++ src/main/java/pro/ivashchuk/employerresearcher/controller/PDFController.java
@@ -1,8 +1,12 @@
 package pro.ivashchuk.employerresearcher.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import pro.ivashchuk.employerresearcher.service.PDFService;
 
 @Controller
 public class PDFController {
 
+    @Autowired
+    PDFService pdfService;
 }
