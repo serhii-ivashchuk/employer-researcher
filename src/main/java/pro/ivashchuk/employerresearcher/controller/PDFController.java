343427d5a41e55bfe28813c6f15ced0b2e4eb021
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-10-25 18:00:30 +0100

    Add generatePDFResume()
    This method is responsible to call PDFService which returns prepared PDF of resume.

--- src/main/java/pro/ivashchuk/employerresearcher/controller/PDFController.java
+++ src/main/java/pro/ivashchuk/employerresearcher/controller/PDFController.java
@@ -1,12 +1,24 @@
 package pro.ivashchuk.employerresearcher.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import pro.ivashchuk.employerresearcher.service.PDFService;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 @Controller
 public class PDFController {
 
     @Autowired
     PDFService pdfService;
+
+    @GetMapping("/resumes/resume/{id}/generatePDF")
+    public ResponseEntity<InputStreamResource> generatePDFResume(@PathVariable("id") Long id) throws IOException, URISyntaxException {
+         return pdfService.generateResumePDFById(id);
+    }
 }
