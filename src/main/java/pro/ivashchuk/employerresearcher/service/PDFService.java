0b0f5e730d6dd7a5163dc512f34e0afaf6731b27
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-10-27 18:07:55 +0100

    Add JpaResumeRepository jpaResumeRepository
    This class provides PDFService a Resume for future Resume PDF generation.

--- src/main/java/pro/ivashchuk/employerresearcher/service/PDFService.java
+++ src/main/java/pro/ivashchuk/employerresearcher/service/PDFService.java
@@ -1,8 +1,12 @@
 package pro.ivashchuk.employerresearcher.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pro.ivashchuk.employerresearcher.repository.JpaResumeRepository;
 
 @Service
 public class PDFService {
 
+    @Autowired
+    private JpaResumeRepository jpaResumeRepository;
 }
