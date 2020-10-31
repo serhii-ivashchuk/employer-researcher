3b7b6b8e31b8b166f677c4cf89110cd687ba002e
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-10-31 18:01:33 +0100

    Add JpaCoverLetterTemplateRepository jpaCoverLetterTemplateRepository
    This class provides PDFService a CoverLetterTemplate for future Cover Letter PDF generation.

--- src/main/java/pro/ivashchuk/employerresearcher/service/PDFService.java
+++ src/main/java/pro/ivashchuk/employerresearcher/service/PDFService.java
@@ -3,6 +3,7 @@ package pro.ivashchuk.employerresearcher.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pro.ivashchuk.employerresearcher.repository.JpaCoverLetterRepository;
+import pro.ivashchuk.employerresearcher.repository.JpaCoverLetterTemplateRepository;
 import pro.ivashchuk.employerresearcher.repository.JpaResumeRepository;
 import pro.ivashchuk.employerresearcher.repository.JpaResumeTemplateRepository;
 
@@ -17,4 +18,7 @@ public class PDFService {
 
     @Autowired
     private JpaCoverLetterRepository jpaCoverLetterRepository;
+
+    @Autowired
+    private JpaCoverLetterTemplateRepository jpaCoverLetterTemplateRepository;
 }
