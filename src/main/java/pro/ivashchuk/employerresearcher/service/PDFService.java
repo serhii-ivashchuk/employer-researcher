18320def423b51f714e1ca2c3f977e1fde7886af
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-11-04 18:11:03 +0100

    Add generateResumePDFById()
    This method is responsible for resume pdf creation. It takes id from resume and id from resume template. Then it looks entities in repositories. When entities are found resume template entity's prepareResume() method takes resume entity. This method internally cooks pdf document and returns path to the document. This path finally is used in File input stream. Generally generation of PDF ends by returning response entity which has in it's body resource from input stream of pdf data.

--- src/main/java/pro/ivashchuk/employerresearcher/service/PDFService.java
+++ src/main/java/pro/ivashchuk/employerresearcher/service/PDFService.java
@@ -1,11 +1,21 @@
 package pro.ivashchuk.employerresearcher.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import pro.ivashchuk.employerresearcher.domain.Resume;
+import pro.ivashchuk.employerresearcher.domain.pdf.ResumeTemplate;
 import pro.ivashchuk.employerresearcher.repository.JpaCoverLetterRepository;
 import pro.ivashchuk.employerresearcher.repository.JpaCoverLetterTemplateRepository;
 import pro.ivashchuk.employerresearcher.repository.JpaResumeRepository;
 import pro.ivashchuk.employerresearcher.repository.JpaResumeTemplateRepository;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 @Service
 public class PDFService {
@@ -21,4 +31,18 @@ public class PDFService {
 
     @Autowired
     private JpaCoverLetterTemplateRepository jpaCoverLetterTemplateRepository;
+
+    public ResponseEntity<InputStreamResource> generateResumePDFById(Long resumeId, Long templateId) throws IOException {
+        Resume resume = jpaResumeRepository.findById(resumeId).get();
+        ResumeTemplate resumeTemplate = jpaResumeTemplateRepository.findById(templateId).get();
+        String file_path = resumeTemplate.preparePDF(resume);
+        File file = new File(file_path);
+        InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION,
+                        "attachment;filename="
+                                + file.getName())
+                .contentType(MediaType.APPLICATION_PDF).contentLength(file.length())
+                .body(resource);
+    }
 }
