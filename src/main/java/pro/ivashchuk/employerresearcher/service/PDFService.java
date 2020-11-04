d6b05ac66b5936eae22b78e7ba2e85a9d19da70c
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-11-04 18:12:34 +0100

    Add generateCoverLetterPDFById()
    This method is responsible for cover letter pdf creation. It takes id from cover letter and id from cover letter template. Then it looks entities in repositories. When entities are found cover letter template entity's prepareCoverLetter() method takes cover letter entity. This method internally cooks pdf document and returns path to the document. This path finally is used in File input stream. Generally generation of PDF ends by returning response entity which has in it's body resource from input stream of pdf data.

--- src/main/java/pro/ivashchuk/employerresearcher/service/PDFService.java
+++ src/main/java/pro/ivashchuk/employerresearcher/service/PDFService.java
@@ -6,7 +6,9 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import pro.ivashchuk.employerresearcher.domain.CoverLetter;
 import pro.ivashchuk.employerresearcher.domain.Resume;
+import pro.ivashchuk.employerresearcher.domain.pdf.CoverLetterTemplate;
 import pro.ivashchuk.employerresearcher.domain.pdf.ResumeTemplate;
 import pro.ivashchuk.employerresearcher.repository.JpaCoverLetterRepository;
 import pro.ivashchuk.employerresearcher.repository.JpaCoverLetterTemplateRepository;
@@ -36,6 +38,21 @@ public class PDFService {
         Resume resume = jpaResumeRepository.findById(resumeId).get();
         ResumeTemplate resumeTemplate = jpaResumeTemplateRepository.findById(templateId).get();
         String file_path = resumeTemplate.preparePDF(resume);
+        File file = new File(file_path);
+        InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION,
+                        "attachment;filename="
+                                + file.getName())
+                .contentType(MediaType.APPLICATION_PDF).contentLength(file.length())
+                .body(resource);
+    }
+
+    public ResponseEntity<InputStreamResource> generateCoverLetterPDFById(Long coverLetterId, Long templateId) throws IOException {
+        CoverLetter coverLetter = jpaCoverLetterRepository.findById(coverLetterId).get();
+        CoverLetterTemplate coverLetterTemplate =
+                jpaCoverLetterTemplateRepository.findById(templateId).get();
+        String file_path = coverLetterTemplate.preparePDF(coverLetter);
         File file = new File(file_path);
         InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
         return ResponseEntity.ok()
