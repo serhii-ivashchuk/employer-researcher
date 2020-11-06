91dce6ce3f64e534de4784a60a36deb8ec3ddb2e
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-11-06 18:15:58 +0100

    Add Settings
    This class is responsible to provide basic app setting needed to generate PDF documents

---
+++ src/main/java/pro/ivashchuk/employerresearcher/domain/pdf/Settings.java
@@ -0,0 +1,8 @@
+package pro.ivashchuk.employerresearcher.domain.pdf;
+
+public class Settings {
+    public static final String APP_PATH = "EmployerResearcherUserData";
+    public static final String EMPLOYERS_PATH = "Employers";
+    public static final String POSTITIONS_PATH = "Positions";
+    public static final String BASE_PATH = APP_PATH + "/" + EMPLOYERS_PATH + "/" + POSTITIONS_PATH;
+}
