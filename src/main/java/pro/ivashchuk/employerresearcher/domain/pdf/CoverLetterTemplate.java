f1d8e491e7e7d13593486b79599c71a42ace550c
serhii-ivashchuk <serhii.ivashchuk@gmail.com>
2020-11-06 18:24:23 +0100

    Add @Entity, @Getter, @Setter, @NoArgsConstructor
    Added JPA and Lombok annotations for persistence and avoiding boiler plate code.

--- src/main/java/pro/ivashchuk/employerresearcher/domain/pdf/CoverLetterTemplate.java
+++ src/main/java/pro/ivashchuk/employerresearcher/domain/pdf/CoverLetterTemplate.java
@@ -1,13 +1,21 @@
 package pro.ivashchuk.employerresearcher.domain.pdf;
 
 import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class CoverLetterTemplate {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Setter(AccessLevel.PROTECTED)
